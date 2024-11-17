a70ba0b1c76679bbe843565e504c2473bea611d4
simbullar <simbullar@gmail.com>
2024-11-17 18:17:37 +0100

    added a window and partially made the progress bar

--- venv.py
+++ venv.py
@@ -1,2 +1,24 @@
-import PySimpleGUI
+import PySimpleGUI as sg 
+import time 
 
+# everything inside the window 
+layout = [  [sg.ProgressBar(100, key='-PROGRESS_BAR-')],
+            [sg.Button('Cancel')] ]
+
+count = 0
+
+window = sg.Window('Hello Example', layout)
+
+
+while True:
+    event, values = window.read()
+
+    # if user closes window or presses cancel
+    if event == sg.WIN_CLOSED or sg.Cancel:
+        break
+#    count = ++1
+#    window['-PROGRESS_BAR-'].update(current_count=count)
+    time.wait(1)
+
+
+window.close()
