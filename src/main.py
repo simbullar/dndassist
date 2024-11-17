85585f759f6acbe5b242a287757bd0a722de76cb
simbullar <simbullar@gmail.com>
2024-11-17 19:33:54 +0100

    basically made the login and the registration

--- src/main.py
+++ src/main.py
@@ -1,24 +1,56 @@
 import PySimpleGUI as sg 
-import time 
-
-# everything inside the window 
-layout = [  [sg.ProgressBar(100, key='-PROGRESS_BAR-')],
-            [sg.Button('Cancel')] ]
-
-count = 0
-
-window = sg.Window('Hello Example', layout)
 
 
+#TODO basically a test of my idea of autheticating trough a txt 
+#TODO so create a txt with username and password
+#if you are wondering what purpose did the progress bar serve 
+# idk actually 
+
+
+
+
+#* everything inside the window 
+layoutRegister = [  [sg.Text('Type a username:', size=(16,1)), sg.In(k='-USERNAME-', size=(10,1))],
+                    [sg.Text('Type a password:', size=(16,1)), sg.In(k='-PASS-', size=(10,1))],
+                    [sg.Ok(), sg.Cancel()] ]
+
+layoutLogin= [  [sg.Text('Type your username:', size=(18,1)), sg.In(k='-USERNAME-', size=(10,1))],
+                    [sg.Text('Type your password:', size=(18,1)), sg.In(k='-PASS-', size=(10,1))],
+
+                    [sg.Ok(), sg.Cancel()] ]
+
+try:
+
+    with open("auth_info.txt", "r") as file:
+        content = file.read()
+        print(content)
+
+except FileNotFoundError:
+    with open("auth_info.txt", "w") as file:
+        register = sg.Window('Registration', layoutRegister)
+
+        while True:
+            event, values = register.read()
+
+            if event == 'Ok':
+                username = values['-USERNAME-']
+                password = values['-PASS-']
+                break
+
+            elif event == sg.WIN_CLOSED or 'Cancel':
+                exit()
+        register.close()
+        file.write(str(username)+" /n "+str(password))
+
+
+window = sg.Window('Log In', layoutLogin)
 while True:
     event, values = window.read()
 
-    # if user closes window or presses cancel
-    if event == sg.WIN_CLOSED or sg.Cancel:
+    #* if user closes window 
+    if event == sg.WIN_CLOSED:
         break
-#?    count = ++1
-#?    window['-PROGRESS_BAR-'].update(current_count=count)
-    time.wait(1)
+
 
 
 window.close()
